831675cc09670b9b2772d7c2620564b434e3dd86
Shajjad Shawon <109540869+Shajjad-142@users.noreply.github.com>
2022-10-19 05:01:07 +0600

    Update 116A Tram.cpp

--- Lab -07 (KMDH sir)/116A Tram.cpp
+++ Lab -07 (KMDH sir)/116A Tram.cpp
@@ -1,1 +1,21 @@
 
+//Name: shajjad
+//ID: 21225103142
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a,b,sum=0,s=0;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a>>b;
+        sum=(a+s)-b;
+        if(sum<0)
+            s=0;
+        else
+            s=sum;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
