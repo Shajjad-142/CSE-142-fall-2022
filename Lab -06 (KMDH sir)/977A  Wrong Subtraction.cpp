ebb4bf36f32e2b63c42708f7e88247f66a522de2
Shajjad Shawon <109540869+Shajjad-142@users.noreply.github.com>
2022-10-19 04:55:55 +0600

    Update 977A  Wrong Subtraction.cpp

--- Lab -06 (KMDH sir)/977A  Wrong Subtraction.cpp
+++ Lab -06 (KMDH sir)/977A  Wrong Subtraction.cpp
@@ -1,1 +1,24 @@
 
+//Name: shajjad
+//ID: 21225103142
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k,s;
+    cin>>n>>k;
+    for(int i=0;i<k;i++)
+    {
+        s=n%10;
+        if(s==0)
+        {
+            n=n/10;
+        }
+        else
+        {
+            n=n-1;
+        }
+    }
+    cout<<n<<endl;
+    return 0;
+}
